74c1437361f6ffe4c89afd96e48653889682d751
Hieu Van Phu <hieu.van@pyramid-consulting.com>
2016-05-31 15:07:36 +0700

    Localizable.strings

--- generate.sh
+++ generate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 swiftgen images --output iOS-Architecture/Resource/Generation/Images.swift iOS-Architecture/Resource/Assets.xcassets
-swiftgen strings --output iOS-Architecture/Resource/Generation/Strings.swift iOS-Architecture/Resource/Storyboard/en.lproj/Localizable.strings
+swiftgen strings --output iOS-Architecture/Resource/Generation/Strings.swift iOS-Architecture/Resource/en.lproj/Localizable.strings
 swiftgen storyboards --output iOS-Architecture/Resource/Generation/Storyboards.swift iOS-Architecture/Resource/Storyboard
