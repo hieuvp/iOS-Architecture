fb49c07908a531bf7a97bb1fe87167856c94f5bf
Hieu Van Phu <hieu.van@pyramid-consulting.com>
2016-05-31 15:13:03 +0700

    Home Image Asset

--- iOS-Architecture/Resource/Generation/Images.swift
+++ iOS-Architecture/Resource/Generation/Images.swift
@@ -1,3 +1,17 @@
 // Generated using SwiftGen, by O.Halligon — https://github.com/AliSoftware/SwiftGen
 
-// No image found
+import UIKit
+
+extension UIImage {
+  enum Asset: String {
+    case Home = "Home"
+
+    var image: UIImage {
+      return UIImage(asset: self)
+    }
+  }
+
+  convenience init!(asset: Asset) {
+    self.init(named: asset.rawValue)
+  }
+}
